6c09a87a2982f78f65cd188a91b84886234aff62
nishant129 <nishantborkar139@gmail.com>
2026-05-03 22:35:25 +0530

    some configuration

--- salon/src/App.tsx
+++ salon/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes/AppRoutes';
 import { Loader } from './components/layout/Loader';
+import { ScrollToTop } from './utils/ScrollToTop';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Loader />
       <AppRoutes />
     </BrowserRouter>
--- salon/src/layouts/MainLayout.tsx
+++ salon/src/layouts/MainLayout.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from 'react';
 import { Navbar } from '../components/layout/Navbar';
 import { Footer } from '../components/layout/Footer';
 import { WhatsAppFloat } from '../components/layout/WhatsAppFloat';
-import '../styles/index.css'
+import '../styles/index.css';
 
 interface MainLayoutProps {
   children: ReactNode;
@@ -10,9 +10,9 @@ interface MainLayoutProps {
 
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <div className="min-h-screen bg-cream text-salon-text overflow-x-hidden">
+    <div className="min-h-screen flex flex-col bg-cream text-salon-text overflow-x-hidden">
       <Navbar />
-      <main>{children}</main>
+      <main className="flex-1">{children}</main>
       <Footer />
       <WhatsAppFloat />
     </div>
